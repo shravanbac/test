8fd65641838369ee8ecd6ad380438d05d73224a6
shravanbac <shravanaem12@gmail.com>
2025-09-02 20:45:30 -0400

    added files

---
+++ tools/publish-for-review/publish-for-review.js
@@ -0,0 +1,4 @@
+export default function publishForReview() {
+  // eslint-disable-next-line no-alert
+  alert('✅ Publish-for-review clicked!!!');
+}
---
+++ tools/send-for-review/send-for-review.js
@@ -0,0 +1,4 @@
+export default function sendForReview() {
+  // eslint-disable-next-line no-alert
+  alert('✅ Send-for-review clicked!!!');
+}
